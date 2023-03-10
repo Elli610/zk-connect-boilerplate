41929e53a99c0f19ef2264a5d338ee132c518c3c
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-03-10 11:28:47 +0100

    done

--- back/src/index.ts
+++ back/src/index.ts
@@ -7,12 +7,20 @@ app.use(cors());
 app.use(express.json());
 
 // Instantiate zkConnect with your AppId here
+const zkConnect = new ZkConnect({
+  appId: "0x55c80a16f20a1c1083860d0e83f1a90b",
+});
 
+
+
+// Verify the response
 app.post("/auth", async (req, res) => {
   const { zkConnectResponse } = req.body;
+	const { vaultId } = await zkConnect.verify({zkConnectResponse});
 
   // Verify the zkConnectResponse here and send back the vaultId
-  res.json({ vaultId: "123" });
+  res.json({ vaultId: vaultId });
 });
+
 
 app.listen(process.env.PORT ?? 8080);
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,30 +3,40 @@ import { useEffect, useState } from "react";
 import Theme from "./theme";
 import { ZkConnect, ZkConnectResponse } from "@sismo-core/zk-connect-client";
 import axios from "axios";
+import { DataRequest } from "@sismo-core/zk-connect-client";
+
+
+// The `request` function sends your user to the Sismo vault app to generate the proof.
+
+
+const zkConnect = new ZkConnect({
+  appId: "0x55c80a16f20a1c1083860d0e83f1a90b",
+});
+
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [vaultId, setVaultId] = useState();
 
   useEffect(() => {
-    // const zkConnectResponse = zkConnect.getResponse();
-    // if(zkConnectResponse) {
-    //   setIsLoading(true);
-    //   axios.post('http://localhost:8080/auth', {  })
-    //     .then(res => {
-    //       setIsLoading(false);
-    //       console.log(res.data)
-    //       setVaultId(res.data.vaultId)
-    //     })
-    //     .catch(err => {
-    //       console.log(err.response.data.status);
-    //       setIsLoading(false);
-    //     });
-    // }
+    const zkConnectResponse = zkConnect.getResponse();
+    if(zkConnectResponse) {
+      setIsLoading(true);
+      axios.post('http://localhost:8080/auth', { zkConnectResponse })
+        .then(res => {
+          setIsLoading(false);
+          console.log(res.data)
+          setVaultId(res.data.vaultId)
+        })
+        .catch(err => {
+          console.log(err.response.data.status);
+          setIsLoading(false);
+        });
+    }
   }, []); 
 
   function onButtonClick() {
-    // zkConnect.request();
+    zkConnect.request();
   }
 
   return (
